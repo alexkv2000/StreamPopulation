1a01cc79da602b7322f34fc5072e537ca5e32002
AlexKv2000 <AlexKv2000@mail.ru>
2022-07-04 10:09:02 +0300

    create Stream List of population

---
+++ Education.java
@@ -0,0 +1,8 @@
+package JavaCore_2_2_Streams;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
---
+++ Sex.java
@@ -0,0 +1,6 @@
+package JavaCore_2_2_Streams;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
